4e949d3fd77d8069386ba16900c2347875ad6a34
claudiaazambuja <cclabdis@gmail.com>
2025-07-18 10:54:29 -0300

    fix dtos users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,26 @@
 import { Controller, Post, Body, UseGuards, Request, Patch } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { RegisterDto } from './dto/register.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
 
-  @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
+    @Post('register')
+    async register(@Body() body: RegisterDto) {
     return this.authService.register(body.email, body.password);
-  }
+    }
 
-  @Patch('change-password')
-  async changePassword(
-    @Request() req,
-    @Body() body: { newPassword: string },
-  ) {
+  @Patch('change-password') 
+    async changePassword(@Request() req, @Body() body: ChangePasswordDto  ) {
     return this.authService.changePassword(req.user.userId, body.newPassword);
   }
 }
