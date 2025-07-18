c86887a183a179ef15117d1de6c759202d6a50c2
claudiaazambuja <cclabdis@gmail.com>
2025-07-18 11:24:18 -0300

    fiz users services update

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async createUser(email: string, password: string) {
+    return this.prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+  }
+
+  async updatePassword(id: number, password: string) {
+    return this.prisma.user.update({
+      where: { id },
+      data: { password },
+    });
+  }
+}
